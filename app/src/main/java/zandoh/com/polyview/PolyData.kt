3eaad78770fd6ecee435dc47123e0e617579791c
Dan <link.dohi@gmail.com>
2018-10-15 22:10:40 -0700

    Initial commit

---
+++ app/src/main/java/zandoh/com/polyview/PolyData.kt
@@ -0,0 +1,27 @@
+package zandoh.com.polyview;
+
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.net.CookieHandler
+import java.net.CookieManager
+
+enum class Urls(val url: String) {
+    LOGIN1("https://idp.calpoly.edu/idp/profile/cas/login?service=https://myportal.calpoly.edu/Loginhttps://idp.calpoly.edu/idp/profile/cas/login?service=https://myportal.calpoly.edu/Login"),
+    LOGIN2("")
+}
+
+class PolyData {
+    fun polyLogin(email: String, password: String, queue: RequestQueue) {
+        val cookieManager = CookieManager()
+        CookieHandler.setDefault(cookieManager)
+
+        val req = StringRequest(Request.Method.GET, Urls.LOGIN1,
+            Response.Listener<String> {
+        })
+
+        queue.add(req)
+    }
+}
---
+++ poly_scrape.py
@@ -0,0 +1,43 @@
+import requests
+from http import cookiejar
+
+jar = cookiejar.CookieJar()
+
+def dw(string):
+    f = open("test.html", "w")
+    f.write(string)
+    f.close()
+
+def pp(req):
+    print('{}\n{}\n{}\n\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
+
+sess = requests.Session()
+
+resp = sess.get(
+    "https://idp.calpoly.edu/idp/profile/cas/login?service=https://myportal.calpoly.edu/Login"
+    , cookies=jar, verify=False)
+
+resp2 = sess.post(resp.url,
+    data={
+        "j_username": "zdohnale",
+        "j_password": "***REMOVED***",
+        "_eventId_proceed": ""
+    },
+    headers={
+        "Referer": resp.url,
+        "User-Agent": "Mozilla/5.0",
+        "Upgrade-Insecure-Requests": "1",
+        "Host": "idp.calpoly.edu",
+        "Cache-Control": "max-age=0",
+        "Origin": "https://idp.calpoly.edu",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    }, cookies=jar, verify=False
+)
+
+print(sess.get("https://myportal.calpoly.edu/f/u17l1s6/p/myclasses.u17l1n1696/normal/moodleLinks.resource.uP?terms=2188%2Cmisc&request.preventCache=1538619342920").text)
